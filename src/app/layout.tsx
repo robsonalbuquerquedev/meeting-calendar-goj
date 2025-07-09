3888abbcb2a9dc566095e2bab0bffc7d8ff7fa7f
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-07-08 23:10:49 -0300

    Updating layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Encontros Jovens",
+  title: "GOJ",
   description: "Calendário e gestão de encontros do Grupo de Oração Jovem São Francisco",
 };
 
