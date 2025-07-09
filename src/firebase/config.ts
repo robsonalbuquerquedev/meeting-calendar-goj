13ce4f24a7a777521dc3e1ccc3d900651ca1b10e
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-07-08 23:26:17 -0300

    Updating config.ts file for firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-IXUoW8tHsrmbchQNshm-lvRdBlNGFfo",
-    authDomain: "meeting-calendar-850d6.firebaseapp.com",
-    databaseURL: "https://meeting-calendar-850d6-default-rtdb.firebaseio.com",
-    projectId: "meeting-calendar-850d6",
-    storageBucket: "meeting-calendar-850d6.firebasestorage.app",
-    messagingSenderId: "364349027722",
-    appId: "1:364349027722:web:79fa2ecf9e42b72c0e4473",
-    measurementId: "G-MMZDDX4Y2R"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
